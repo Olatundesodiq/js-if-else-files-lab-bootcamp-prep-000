1026c1a576bc1743d7ec3509b83e86ec81833390
olatundesodiq@gmail.com <olatundesodiq@gmail.com>
2018-07-27 10:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ if (name === "Susan") {
 }else {
     console.log("Expected the name to be Susan")
 }
+if (height === "70") {
+  console.log("the height is correct")
+}else {
+    console.log("Expected the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
